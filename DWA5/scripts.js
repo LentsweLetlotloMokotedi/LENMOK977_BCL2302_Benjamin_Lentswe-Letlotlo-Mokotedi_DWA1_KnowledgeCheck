d1a9123fb41e2fc10cd5a055b63919cfd8f34da7
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-06-06 16:36:47 +0200

    Update scripts.js

--- DWA5/scripts.js
+++ DWA5/scripts.js
@@ -1,50 +1,31 @@
-// scripts.js
-
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
-
-// User Story 1: Starting program state
-result.innerText = "NO calculation performed";
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
-  // User Story 3: Validation when values are missing
   if (dividend === "" || divider === "") {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
     return;
   }
-
-  // User Story 4: An invalid division should log an error in the console
   if (!isNumeric(dividend) || !isNumeric(divider)) {
     console.error("Something critical went wrong. Please reload the page");
-    result.innerText = "Division not performed. Invalid number provided. Try again";
+    result.innerText = "Division not perfomed. Invalid number provided. Try again";
     return;
   }
-
-  // User Story 5: Providing anything that is not a number should crash the program
-  try {
-    if (divider === "0") {
-      throw new Error("Division by zero is not allowed");
-    }
-
-    const quotient = dividend / divider;
-    if (isWholeNumber(quotient)) {
-      // User Story 2: Dividing numbers result in a whole number
-      result.innerText = quotient;
-    } else {
-      result.innerText = Math.floor(quotient);
-    }
-  } catch (error) {
-    console.error(error);
-    result.innerText = "Division not performed. Invalid number provided. Try again";
+  const quotient = dividend / divider;
+  if (isWholeNumber(quotient)) {
+    result.innerText = quotient;
+  } else {
+    result.innerText = Math.floor(quotient);
   }
 });
-
-const isNumeric = (value) => !isNaN(value);
-
+const isNumeric = value => {
+    const numericValue = Number(value);
+    return typeof numericValue === 'number' && !isNaN(numericValue) && Number.isInteger(numericValue) && numericValue >= 0;
+  };
+  
+  
 function isWholeNumber(value) {
   return Number.isInteger(value);
 }
