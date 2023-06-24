5d316a3f6dec1fee36b8447cd0233fbec942df77
LentsweLetlotloMokotedi <lentsweletlotlo0@gmail.com>
2023-06-24 14:51:20 +0200

    DWA11

--- DWA11/script.js
+++ DWA11/script.js
@@ -1,47 +1,142 @@
+// reduxScript.js
+
+// Define constants
 const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+const STEP_AMOUNT = 1;
+
+// Define action types
+const ActionTypes = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  RESET: "RESET",
+};
+
+// Define action creators
+function incrementCounter() {
+  return { type: ActionTypes.INCREMENT };
+}
+
+function decrementCounter() {
+  return { type: ActionTypes.DECREMENT };
+}
+
+function resetCounter() {
+  return { type: ActionTypes.RESET };
+}
+
+// Define reducer function
+function counterReducer(state = 0, action) {
+  switch (action.type) {
+    case ActionTypes.INCREMENT:
+      return state + STEP_AMOUNT;
+    case ActionTypes.DECREMENT:
+      return state - STEP_AMOUNT;
+    case ActionTypes.RESET:
+      return 0;
+    default:
+      return state;
+  }
+}
+
+// Create a Redux store
+const store = Redux.createStore(counterReducer);
+
+// Update the UI with the initial state
+let currentValue = store.getState();
+function updateUI() {
+  currentValue = store.getState();
+  number.value = currentValue.toString();
+
+  if (currentValue <= MIN_NUMBER) {
+    subtract.disabled = true;
+  } else if (currentValue >= MAX_NUMBER) {
+    add.disabled = true;
+  } else {
+    subtract.disabled = false;
+    add.disabled = false;
+  }
+
+  console.log(currentValue);
+}
+
+// Subscribe to state changes and update the UI
+store.subscribe(updateUI);
+
+// Attach event listeners to the buttons
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - 1;
-  number.value = newValue;
-  if (add.disabled === true) {
-    add.disabled = false;
-  }
-  if (newValue <= MIN_NUMBER) {
-    subtract.disabled = true;
-  }
 
-  console.log(number.value);
-}
+subtract.addEventListener("click", () => {
+  store.dispatch(decrementCounter());
+});
 
-
-const addHandler = () => {
-  const newValue = parseInt(number.value) + 1;
-  number.value = newValue;
-  if (subtract.disabled === true) {
-    subtract.disabled = false;
-  }
-  if (newValue >= MAX_NUMBER) {
-    add.disabled = true;
-  }
-  console.log(number.value);
-};
-console.log(number.value);
-subtract.addEventListener('click', subtractHandler);
-add.addEventListener('click', addHandler);
-
+add.addEventListener("click", () => {
+  store.dispatch(incrementCounter());
+});
 
 const resetButton = document.querySelector("sl-button");
-
-const counterValueInput = document.querySelector(".counter_value");
-function handleResetButtonClick() {
-  counterValueInput.value = "0";
+resetButton.addEventListener("click", () => {
+  store.dispatch(resetCounter());
   alert("The counter has been reset.");
-}
-resetButton.addEventListener("click", handleResetButtonClick);
+});
+
+// Initialize the UI
+updateUI();
+
+
+
+
+
+
+
+// const MAX_NUMBER = 15;
+// const MIN_NUMBER = -5;
+// const STEP_AMOUNT = 5;
+// const number = document.querySelector('[data-key="number"]');
+// const subtract = document.querySelector('[data-key="subtract"]');
+// const add = document.querySelector('[data-key="add"]');
+// const subtractHandler = () => {
+//   const newValue = parseInt(number.value) - 1;
+//   number.value = newValue;
+//   if (add.disabled === true) {
+//     add.disabled = false;
+//   }
+//   if (newValue <= MIN_NUMBER) {
+//     subtract.disabled = true;
+//   }
+
+//   console.log(number.value);
+// }
+
+
+// const addHandler = () => {
+//   const newValue = parseInt(number.value) + 1;
+//   number.value = newValue;
+//   if (subtract.disabled === true) {
+//     subtract.disabled = false;
+//   }
+//   if (newValue >= MAX_NUMBER) {
+//     add.disabled = true;
+//   }
+//   console.log(number.value);
+// };
+// console.log(number.value);
+// subtract.addEventListener('click', subtractHandler);
+// add.addEventListener('click', addHandler);
+
+
+// const resetButton = document.querySelector("sl-button");
+
+// const counterValueInput = document.querySelector(".counter_value");
+// function handleResetButtonClick() {
+//   counterValueInput.value = "0";
+//   alert("The counter has been reset.");
+// }
+// resetButton.addEventListener("click", handleResetButtonClick);
+
+
 
 
 
